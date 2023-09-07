39178c3b13e24793221d7da8188d79115f9e7cc0
Goran Boban <gboban70@gmail.com>
2023-09-07 12:32:42 +0200

    Refactor code, add comments, and include .env.example file

--- docx_helper.js
+++ docx_helper.js
@@ -29,29 +29,37 @@ module.exports = class DOCXHelper {
     async getDocumentStats ( callback ){
         await officeParser.parseOfficeAsync(this._fileBuffer)
         .then((data) => {
-          var stats = {}
+          const stats = {
+            characters: 0,
+            words: 0,
+            sentences: 0,
+            frequencies: []
+          }
 
           stats.characters = 0
+          // count non-special characters in text
           lodash.forEach(data, (value, key) => {
             if(!(" !\"#$%&'()*+,-./:;?@[\]^_`{|}~\n\r\t").indexOf(value)){
                 ++stats.characters
             }
           })
-          var words = lodash.words(data)
-          stats.words = words.length
+
+          // number of words
+          stats.words = lodash.words(data).length
+
+          // number of sentences
           stats.sentences = data.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|").length
-          var frequencies = {}
+
+          // calculate word frequencies
+          const frequencies = {}
           words.forEach((value, key) =>{
-            var lowerCaseValue = value.toString().toLowerCase()
-            if(
-                (frequencies[lowerCaseValue] === undefined) 
-                || (frequencies[lowerCaseValue] === null)
-            ) frequencies[lowerCaseValue] = 0
-            ++frequencies[lowerCaseValue]
+            const lowerCaseValue = value.toString().toLowerCase()
+            if((frequencies[lowerCaseValue] === undefined) || (frequencies[lowerCaseValue] === null)) 
+              frequencies[lowerCaseValue] = 0
+              ++frequencies[lowerCaseValue]
           })
           const map = new Map(Object.entries(frequencies));
-          var sortedFrequencies = [...map.entries()].sort((a, b) => b[1] - a[1])
-          stats.frequencies = sortedFrequencies
+          stats.frequencies = [...map.entries()].sort((a, b) => b[1] - a[1])
         
           callback(null, stats)
         })
